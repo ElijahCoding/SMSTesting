865a34807493970237115d4b5980d120c0251fa6
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-19 10:34:12 +0300

    input built

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,32 @@
 <template>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <form @submit.prevent="submit">
+                        <div class="form-group">
+                            <vue-tel-input v-model="phone"
+                                           class="form-control"
+                                           :preferredCountries="['us', 'gb', 'ru']">
+                                           >
+                            </vue-tel-input>
 
+                        </div>
+
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-dark">
+                                Send
+                            </button>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import 'vue-tel-input/dist/vue-tel-input.css'
+    import VueTelInput from 'vue-tel-input'
+
     export default {
         data () {
             return {
@@ -10,9 +34,13 @@
             }
         },
 
+        components: {
+            VueTelInput
+        },
+
         methods: {
             submit () {
-                
+                console.log(this.phone);
             }
         }
 
