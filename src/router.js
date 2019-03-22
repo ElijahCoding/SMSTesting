192996d508b423ae02b9e2795b94b0f18bf9a8e0
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-22 10:51:04 +0300

    building iframe-test

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home'
+import Iframe from './views/Iframe'
 
 Vue.use(Router)
 
@@ -8,10 +9,15 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
+        path: '/',
+        name: 'iframe-test',
+        component: Iframe
+    }
   ]
 })
---
+++ src/views/Iframe.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <form @submit.prevent="submit">
+                    <div class="form-group">
+                        <textarea name="name"
+                                  class="form-control"
+                                  required
+                                  placeholder="Token"
+                                  >
+                        </textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text"
+                               required
+                               class="form-control"
+                               placeholder="ID"
+                               >
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text"
+                               required
+                               class="form-control"
+                               placeholder="FirstName"
+                               >
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text"
+                               required
+                               class="form-control"
+                               placeholder="LastName"
+                               >
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email"
+                               required
+                               class="form-control"
+                               placeholder="Email"
+                               >
+                    </div>
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-dark">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+
+    }
+</script>
